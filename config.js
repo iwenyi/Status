b7ad9ad4242c15e198113861dbbd4ea91e1d4327
wenyi <63294559+iwenyi@users.noreply.github.com>
2023-09-13 19:22:15 +0800

    Update

---
+++ config.js
@@ -0,0 +1,30 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Wenyi Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm787532343-04cfbb2244fa0f15eb1c9c12',
+    'm788829650-ec8421f860279ed450cf13d9',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Blog',
+      url: 'https://www.wenyi.cc/'
+    },
+    {
+      text: 'HomePage',
+      url: 'https://i.wenyi.cc/'
+    },
+  ],
+};
